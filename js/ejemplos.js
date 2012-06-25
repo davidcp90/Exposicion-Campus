0f2d201a703726049eec13fcef3599ce86148b45
David <davidcp90@gmail.com>
2012-06-24 23:40:14 -0500

    Una manada de cambios
    YA quedo mi part today hecha le cambia el tema del highlight peru no
    pure lograr sue cuando iso codigo HTML o XML me salga dentro de la
    etiquette code de la maenra sue deberia , tambien agree unos pequeños
    css y le cambia el color por que ya he vista a area genre utilizando el
    reveal incluso a John Acosta

---
+++ js/ejemplos.js
@@ -0,0 +1,25 @@
+/**
+ * @author David Felipe Camargo Polo
+ */
+
+ window.onload = function() {
+        var canvas = document.getElementById('canvasEj');
+        var context = canvas.getContext('2d');
+
+        context.beginPath();
+        context.rect(0, 0, 200, 100);
+        // create radial gradient
+        var grd = context.createRadialGradient(138, 50, 10, 138, 50, 200);
+        // light blue
+        grd.addColorStop(0, "#8E66FF");
+        // dark blue
+        grd.addColorStop(1, "#094CB3");
+        context.fillStyle = grd;
+        context.fill();
+        context.font = "40px Arial";
+        context.fillStyle = "white";
+        context.fillText("Canvas", 40, 60);
+        context.lineWidth = 5;
+        context.strokeStyle = 'black';
+        context.stroke();
+      };
